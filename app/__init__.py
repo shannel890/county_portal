8b8b0c415e8a77928e42c1c8c08340dc88b8c0b1
shannel890 <shannelkirui739@gmail.com>
2025-05-30 16:10:26 +0300

    modified app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,28 +1,27 @@
 from flask import Flask
 from flask_security import SQLAlchemyUserDatastore
-from app.extension import db,migrate,mail,security
+from app.extension import db, mail, security
 from app.models.user import User, Role
+from flask_security import hash_password
 from config import Config
-from flask_migrate import Migrate
+import uuid
+# Import necessary modules and blueprints
 from app.main.views import main_bp
 from app.api.routes import api_bp
+from app.auth.routes import auth_bp
 # / blueprint import
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     
     db.init_app(app)
-    
-    migrate.init_app(app,db)
     mail.init_app(app)
     
 
     #register blueprints
     app.register_blueprint(main_bp)
-
-    #register blueprints
     app.register_blueprint(api_bp)
-
+    app.register_blueprint(auth_bp)
     # Import models and intialize Flask-Security
     user_datastore = SQLAlchemyUserDatastore(db, User, Role)
     security.init_app(app,user_datastore)
@@ -30,5 +29,32 @@ def create_app():
     
     with app.app_context():
         db.create_all()
+
+        roles_data = [{'name': 'super_admin', 'description': 'Administrator role'},
+                      {'name': 'staff', 'description': 'county staff with limited access'},
+                      {'name': 'citizen', 'description': 'citizen with basic data access'},
+                      {'name': 'guest', 'description': 'guest with minimal access'}]
+        for role_data in roles_data:
+            role = Role.query.filter_by(name=role_data['name']).first()
+            if not role:
+                role = Role(**role_data)
+                db.session.add(role)
+        db.session.commit()
+
+        admin_role = Role.query.filter_by(name='super_admin').first()
+        admin_user = User.query.filter_by(email='shannel@gmail.com').first()
+
+        if not admin_user:
+            admin_user = User(
+                email='shannel@gmail.com',
+                password=hash_password('shannel254'),  # Use a hashed password in production
+                active=True,
+                roles=[admin_role],
+                fs_uniquifier=str(uuid.uuid4())
+            )
+            db.session.add(admin_user)
+        db.session.commit()
+        print("Admin user created successfully.")
+
     return app
     
