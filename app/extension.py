679df836c364fbb3924eae1184390ec7bdc497e6
shannel890 <shannelkirui739@gmail.com>
2025-05-30 14:16:54 +0300

    modified app/extension.py

--- app/extension.py
+++ app/extension.py
@@ -1,5 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_security import Security
+from flask_mail import Mail
 
 db = SQLAlchemy()
 migrate = Migrate()
+security = Security()
+mail = Mail()
