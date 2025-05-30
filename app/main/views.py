585612ec518d4b82e1ff35d43dc2ee778ad0e7f0
shannel890 <shannelkirui739@gmail.com>
2025-05-30 16:14:04 +0300

    modified app/main/views.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,19 @@
 from flask import Blueprint
+from flask_security import login_required, current_user,roles_required
 
 main_bp = Blueprint('main_bp',__name__)
 
 @main_bp.route('/')
+@login_required
 def home():
-    return "Welcome to the County portal!"
+    return "Welcome to the County portal "
+
 
 @main_bp.route('/about')
 def about():
     return "This is the about page of the County portal."
+
+@main_bp.route('/dashboard')
+@roles_required('super_admin')
+def dashboard():
+    return "This dashboard is accessible to admins only"
