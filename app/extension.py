7b071c66b1e0e4492271e0255996a33c08f4d0af
shannel890 <shannelkirui739@gmail.com>
2025-06-06 12:07:19 +0300

    modified app/extension.py

--- app/extension.py
+++ app/extension.py
@@ -2,8 +2,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_security import Security
 from flask_mail import Mail
+from flask_wtf.csrf import CSRFProtect
 
 db = SQLAlchemy()
 migrate = Migrate()
 security = Security()
 mail = Mail()
+csrf = CSRFProtect()
